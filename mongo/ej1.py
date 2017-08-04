f95e3bbb06b651ae85961f4a0acfaf58a14bf2b0
Alan Badillo <badillo.soft@hotmail.com>
2017-08-04 10:54:06 -0500

    Sesión 5 - Mongo Ej1

---
+++ mongo/ej1.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+cli = MongoClient()
+
+db = cli.cisco # cli["cisco"]
+
+# personas = db["personas"]
+
+db.personas.insert_one({
+    "nombre": "Python",
+    "edad": 12,
+})
+
+db.personas.insert_many([
+    {
+        "nombre": "Persona 1",
+        "edad": 15,
+    },
+    {
+        "nombre": "Persona 2",
+        "edad": 16,
+    },
+    {
+        "nombre": "Persona 3",
+        "edad": 17,
+    },
+])
+
+cli.close()
